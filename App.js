1e62a5047dc49f59ef869719a38b02ff659970d1
Aayasha Khatun <167515078+Aayasha786@users.noreply.github.com>
2024-07-29 16:07:57 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import TodoLogic from './component/TodoLogic';
+
+
+
+
+function App() {
+  return (
+    //<>
+    <TodoLogic/>
+      
+    //</>
+  
+  );
+}
+
+export default App;
